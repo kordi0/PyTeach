8a9b125946b01bfaf80287800f5e63447d8486ea
kordi0 <korolevdmitrii@gmail.com>
2018-01-29 23:52:51 +0300

    I am stupid(

--- kkk.py
+++ kkk.py
@@ -1,1 +1,15 @@
-print("Hello world!")
+import sys
+x = input("a: ")
+y = input("b: ")
+
+def chek(a, b):
+    if type(int(a)) and type(int(b)) != int:
+        return("str")
+    else:
+        if int(a) > int(b):
+            return(">")
+        elif int(a) < int(b):
+            return("<")
+        else:
+            return("=")
+print(chek(1, 2))
